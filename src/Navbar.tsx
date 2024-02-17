b3cb42ea4d18309d077f293f021e02aee9603276
1jds <jamesdstacey2@gmail.com>
2024-02-17 14:51:32 +1100

    adding login dialog

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,13 +1,34 @@
+import { MouseEventHandler, useRef } from "react";
+
 function Navbar() {
+  const loginModalRef = useRef<HTMLDialogElement>(null);
+
+  const toggleLoginDialog = () => {
+    if (!loginModalRef) {
+      return;
+    }
+    loginModalRef.current?.hasAttribute("open")
+      ? loginModalRef.current.close()
+      : loginModalRef.current?.showModal();
+  };
+
   return (
-    <nav className="navbar">
-      <p>BAR BUDDIES</p>
-      {/* if authed = Username My Plans Logout  else = Login*/}
-      <a href="#">User Name</a>
-      <a href="#">My Plans</a>
-      <a href="#">Logout</a>
-      {/* <a href="#">Login</a> */}
-    </nav>
+    <>
+      <nav className="navbar">
+        <p>BAR BUDDIES</p>
+        {/* if authed = Username My Plans Logout  else = Login*/}
+        <a href="#">User Name</a>
+        <a href="#">My Plans</a>
+        <a onClick={toggleLoginDialog} href="#">
+          Logout
+        </a>
+        {/* <a href="#">Login</a> */}
+        <dialog ref={loginModalRef}>
+          <button autoFocus>Close</button>
+          <p>This is our funky dialog!!</p>
+        </dialog>
+      </nav>
+    </>
   );
 }
 
