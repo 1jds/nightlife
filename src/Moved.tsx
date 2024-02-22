077f7da5945c5378b55b7932d3e9d3b39a6db1d6
1jds <jamesdstacey2@gmail.com>
2024-02-22 21:24:45 +1100

    set up redirect page

---
+++ src/Moved.tsx
@@ -0,0 +1,26 @@
+export default function Moved() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        background: "#0a0a23",
+        color: "#f5f6f7",
+      }}
+    >
+      <h1>The front-end for this project has moved!</h1>
+      <h2 style={{ fontWeight: "500" }}>
+        Please visit the following link:{" "}
+        <a
+          style={{ color: "#198eee" }}
+          href="https://nightlife-8ddy.onrender.com"
+        >
+          https://nightlife-8ddy.onrender.com
+        </a>
+      </h2>
+    </div>
+  );
+}
