494b04432b6b60f1dd07c618dbe257182fd00b37
1jds <jamesdstacey2@gmail.com>
2024-02-22 21:33:11 +1100

    rolled back Moved.tsx so I can keep developing the actual front-end

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Moved from "./Moved.tsx";
-// import App from "./App.tsx";
+// import Moved from "./Moved.tsx";
+import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Moved />
+    <App />
   </React.StrictMode>
 );
